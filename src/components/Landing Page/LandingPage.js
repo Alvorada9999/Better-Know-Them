bb89e5eb310c71f9ce93cec4f85f71b017249ee4
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-11-05 18:58:22 -0300

    Update for create and enter room mechanics

---
+++ src/components/Landing Page/LandingPage.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import enterRoom from "./enterRoom";
+import createRoom from "./createRoom";
+
+import "../../styles/landingPage.css";
+
+function LandingPage(props) {
+  const [roomName, setRoomName] = useState("");
+
+  function updateRoomName(event) {
+    setRoomName(event.target.value);
+  }
+
+  function makeEnterRoom(event){
+    event.preventDefault();
+    enterRoom(props, roomName);
+  }
+
+  function makeCreateRoom(event){
+    event.preventDefault();
+    createRoom(props, roomName);
+  }
+
+  return (
+    <div className="container">
+      <p className="title">Better Know Them</p>
+      <form>
+        <input
+          className="textField"
+          type="text"
+          placeholder="Room Name"
+          onChange={updateRoomName}
+        ></input>
+        <button onClick={makeEnterRoom}>Enter Room</button>
+        <button onClick={makeCreateRoom}>Create Room</button>
+      </form>
+      <p className="instructions">
+        After of the creation of your room, give it's name to your friends.
+      </p>
+    </div>
+  );
+}
+
+export default LandingPage;
